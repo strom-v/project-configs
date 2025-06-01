3d3eb5d031f5a6aeb1e4acef03851fb98185ed85
strom-v <strom.vitaly@gmail.com>
2025-06-01 18:25:51 +0300

    Update .eslint.config.ts

--- eslint/react/.eslint.config.ts
+++ eslint/react/.eslint.config.ts
@@ -13,7 +13,7 @@ export default [
       },
     },
     rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }],
     },
     ignores: ['node_modules/', 'dist/', 'build/', 'coverage/'],
   },
